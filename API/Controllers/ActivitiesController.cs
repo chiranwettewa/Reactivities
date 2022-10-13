0226d0a8c1ba09215c492353801cc7483c672ce6
chiranwettewa <chiranwettewa@gmail.com>
2022-10-13 21:59:53 +0530

    mediatR

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -13,10 +13,10 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    
+
     public class ActivitiesController : BaseApiController
     {
-      
+
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
@@ -27,9 +27,23 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Detaills.Query { Id = id });
         }
-
-
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid Id,Activity activity)
+        {
+            activity.Id = Id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+        }
     }
 }
